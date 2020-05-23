7f3b344d9077e6a6230eb05ad6cf58999b02679d
ashitani0803 <ashitani0803@gmail.com>
2020-05-23 17:46:55 +0000

    [delete] links_contoroller update commentout

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -32,33 +32,9 @@ class LinksController < ApplicationController
   end
 
   def update
-    # if @link.update(link_params)
-    #   # tag_linksのtag_idは 1, 2, 3
-    #   # tag_idsは 3, 4, 5
-    #   tag_links = @link.tag_links
-    #   # tag_linkを作成(新規でパラメータが送られてきたら)
-    #   @link.tag_ids.each do |t|
-    #     if !tag_links.find_by(link_id: @link.id, tag_id: t.to_i)   
-    #       tag_link = TagLink.new(link_id: @link.id, tag_id: t.to_i)
-    #       tag_link.save
-    #       @link.tag_ids.delete("t")
-    #     # tag_linkを削除(既存のパラメータが送られてこなかったら)
-    #     elsif 
-    #       array = tag_links.pluck(:tag_id)
-    #       array.each do |tl|
-    #         TagLink.find_by(link_id: @link.id, tag_id: tl.to_i).destroy
-    #       end
-    #     end
-    #     p @link.tag_ids
-    #     p array
-    #   end
-    #   redirect_to new_link_path
-    # else
-    #   render :edit
-    # end
     if @link.update(link_params)
-      # tag_linksのtag_idは 1, 2, 3
-      # tag_idsは 3, 4, 5
+      # tag_linksのtag_idは 1, 2, 3と仮定
+      # tag_idsは 3, 4, 5と仮定
       tag_links = @link.tag_links # ["1", "2", "3"]
       # tag_linkを作成(新規でパラメータが送られてきたら)
       @link.tag_ids.each do |t| # ["3", "4", "5"]

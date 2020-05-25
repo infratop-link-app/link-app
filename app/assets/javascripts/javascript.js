5bf1b555c0b3457918838d515b4c20703f6ad059
ashitani0803 <ashitani0803@gmail.com>
2020-05-25 17:59:33 +0000

    [add] tag-search-func

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -1,10 +1,10 @@
-$(function () {
+$(document).on("turbolinks:load", function () {
     $(".tab-content > div").not("#tab1").hide()
     // .not()で除外
     $(".tab-buttons span").click(function () {
-        var thisclass = $(this).attr("class")
+        let thisclass = $(this).attr("class")
         // 上記だとthisにあるclassを取得する
-        $("#lamp").removeClass().addClass("#lamp").addClass(thisclass)
+        $("#lamp").removeClass().addClass(thisclass)
         $(".tab-content>div").each(function () {
             if ($(this).hasClass(thisclass)) {
                 // 要素集合全てのうちから、引数に指定したクラスを持つ要素がひとつでもあればtrueを返す。

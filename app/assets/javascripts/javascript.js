ea5fcaaa3639fdc75947636e2494e75b29fc3ca9
vagrant <vagrant@localhost.localdomain>
2020-06-01 13:05:55 +0000

    [Add]book_list_function

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -93,7 +93,7 @@ $(document).on("turbolinks:load", function () {
         let array = JSON.parse(localStorage.getItem("fav_links"))
         for (key in array) {
             $(".list-container").append(
-                `<li><a href="${array[key].url}">${array[key].title}</a></li>`
+                `<li class= "book_list" ><a href="${array[key].url}">${array[key].title}</a></li>`
             )
         }
     }
